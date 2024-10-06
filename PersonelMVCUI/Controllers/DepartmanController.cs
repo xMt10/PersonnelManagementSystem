bd5b7f005a715635e996d1a94c34f5be262840ff
Mahmut Turpçu <2020556059@ogr.cu.edu.tr>
2024-10-07 01:36:00 +0300

    Add project files.

---
+++ PersonelMVCUI/Controllers/DepartmanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonelMVCUI.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        // GET: Departman
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
