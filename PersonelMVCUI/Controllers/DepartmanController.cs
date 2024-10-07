2c4e8ef61f73af22af994b726f1203f21589bcd2
Mahmut Turpçu <2020556059@ogr.cu.edu.tr>
2024-10-07 16:01:07 +0300

    Connected to the database with Entity Framework and completed CRUD operations for the departments.

--- PersonelMVCUI/Controllers/DepartmanController.cs
+++ PersonelMVCUI/Controllers/DepartmanController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using PersonelMVCUI.Models.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +10,58 @@ namespace PersonelMVCUI.Controllers
 {
     public class DepartmanController : Controller
     {
+        PersonelDbEntities db = new PersonelDbEntities();
         // GET: Departman
         public ActionResult Index()
         {
-            return View();
+            var model = db.Departman.ToList();
+            return View(model);
         }
+
+        [HttpGet]
+        public ActionResult Yeni()
+        {
+            return View("DepartmanForm");
+        }
+
+        [HttpPost]
+        public ActionResult Kaydet(Departman departman)
+        {
+            if(departman.Id == 0)
+            {
+                db.Departman.Add(departman);
+            }
+            else
+            {
+                var guncellenecekDepartman = db.Departman.Find(departman.Id);
+                if (guncellenecekDepartman == null)
+                    return HttpNotFound();
+                guncellenecekDepartman.Ad = departman.Ad;    
+            }
+            
+            db.SaveChanges();
+            return RedirectToAction("Index", "Departman");
+        }
+
+
+        public ActionResult Guncelle(int id)
+        {
+            var model = db.Departman.Find(id);
+            if (model == null)
+                return HttpNotFound();
+            return View("DepartmanForm",model);
+        }
+
+        public ActionResult Sil(int id)
+        {
+            var silinecekDepertman = db.Departman.Find(id);
+            if(silinecekDepertman == null)
+                return HttpNotFound();
+            db.Departman.Remove(silinecekDepertman);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Departman");
+
+        }
+
     }
 }
